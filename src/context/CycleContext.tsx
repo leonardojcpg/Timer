6a63664711da13c795387f83d9999fb327d22568
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-02-25 20:57:44 -0300

    saving data to localStorage

--- src/context/CycleContext.tsx
+++ src/context/CycleContext.tsx
@@ -1,4 +1,11 @@
-import { createContext, ReactNode, useReducer, useState } from 'react'
+import { differenceInSeconds } from 'date-fns'
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react'
 import {
   addNewCycleAction,
   interruptCurrentCycleAction,
@@ -30,17 +37,38 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (inicialState) => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@timer-pomodoro:cycles-state-1.0.0',
+      )
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON)
+      }
+      return inicialState
+    },
+  )
 
   const { cycles, activeCycleId } = cyclesState
-
-  // const [activeCycleId, setActiveCycleId] = useState<String | null>(null)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+    return 0
+  })
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+    // ao salvar no localStorage, colocamos o "@nomeDoProjeto:nomeDaInformaçao-versao(exemplo: 1.0.0)"
+    localStorage.setItem('@timer-pomodoro:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
@@ -63,8 +91,6 @@ export function CyclesContextProvider({
     dispatch(addNewCycleAction(newCycle))
     // setCycles((state) => [...cycles, newCycle])
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
